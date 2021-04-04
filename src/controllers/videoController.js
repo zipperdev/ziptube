2cc197e315be4ae141bc176c5503f3c24f065498
zipperdev <sh20101119@gmail.com>
2021-04-04 20:45:19 +0900

    Update beta 1.12.0
    Add pug templates and add all routers and controllers.

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,34 @@
-export const trending = (req, res) => res.send("Home");
+export const trending = (req, res) => {
+    const videos = [
+        {
+            title:"First Video", 
+            rating: 5, 
+            comments: 2, 
+            createdAt: "2 minutes ago", 
+            views: 1000, 
+            id: 1 
+        }, 
+        {
+            title:"Second Video", 
+            rating: 5, 
+            comments: 5, 
+            createdAt: "4 minutes ago", 
+            views: 10000, 
+            id: 2 
+        }, 
+        {
+            title:"Third Video", 
+            rating: 5, 
+            comments: 10, 
+            createdAt: "10 minutes ago", 
+            views: 100000, 
+            id: 3 
+        }
+    ];
+    return res.render("home", { pageTitle: "Home", videos });
+};
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 export const search = (req, res) => res.send("Search");
-export const see = (req, res) => res.send(`Watch Video #${req.params.id}`);
 export const upload = (req, res) => res.send("Upload");
-export const edit = (req, res) => res.send("Edit");
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const remove = (req, res) => res.send("Delete");
