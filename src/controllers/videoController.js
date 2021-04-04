a02424d60014e60547bb342ff4479bb3bf8ad88d
zipperdev <sh20101119@gmail.com>
2021-04-04 14:11:35 +0900

    Update beta 1.11.0
    Add more routes.

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,3 +1,6 @@
 export const trending = (req, res) => res.send("Home");
-export const watch = (req, res) => res.send("Watch");
-export const edit = (req, res) => res.send("Edit Video");
+export const search = (req, res) => res.send("Search");
+export const see = (req, res) => res.send(`Watch Video #${req.params.id}`);
+export const upload = (req, res) => res.send("Upload");
+export const edit = (req, res) => res.send("Edit");
+export const remove = (req, res) => res.send("Delete");
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { watch, edit } from "../controllers/videoController";
+import { see, upload, edit, remove } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/watch", watch);
-videoRouter.get("/edit", edit);
+videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/remove", remove);
+videoRouter.get("/upload", upload);
 
 export default videoRouter;
